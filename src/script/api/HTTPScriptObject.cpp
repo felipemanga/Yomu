b8dd1105a23a4927d0bada49b90f5de204f7304e
Felipe Manga <felipe.manga@MacBook-Pro.Home>
2022-05-29 14:11:00 +0100

    Support POST http requests

--- src/script/api/HTTPScriptObject.cpp
+++ src/script/api/HTTPScriptObject.cpp
@@ -6,12 +6,7 @@
 
 #include <regex>
 
-#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httprequest/httplib.hpp>
-#else
-#include <httprequest/HTTPRequest.hpp>
-#endif
-
 #include <script/Engine.hpp>
 #include <script/ScriptObject.hpp>
 #include <task/TaskManager.hpp>
@@ -26,6 +21,11 @@ public:
         Error
     } state = State::Ready;
 
+    enum class Type {
+        Get,
+        Post
+    };
+
     U32 code = 0;
     Vector<U8> bytes;
     String error;
@@ -33,46 +33,48 @@ public:
     std::shared_ptr<script::Engine> engine;
 
     HTTPScriptObject() {
-        addMethod("get", this, &HTTPScriptObject::get);
-        addProperty("state", [=]{return (int) state;});
-        addProperty("code", [=]{return code;});
-        addProperty("text", [=]{return String{bytes.begin(), bytes.end()};});
-        addProperty("url", [=]{return url;});
-        addProperty("error", [=]{return error;});
-        makeGlobal("http");
+      addMethod("get", this, &HTTPScriptObject::get);
+      addMethod("post", this, &HTTPScriptObject::post);
+      addProperty("state", [=]{return (int) state;});
+      addProperty("code", [=]{return code;});
+      addProperty("text", [=]{return String{bytes.begin(), bytes.end()};});
+      addProperty("url", [=]{return url;});
+      addProperty("error", [=]{return error;});
+      makeGlobal("http");
     }
 
-    script::Value get(const String& url){
+    script::Value get(const String& url) {
+        return request(Type::Get, url);
+    }
+
+    script::Value post(const String& url, const String& body, const String& contentType) {
+        return request(Type::Post, url, body, contentType);
+    }
+
+    script::Value request(Type type, const String& url, const String& body = "", const String& contentType = ""){
         engine = getEngine().shared_from_this();
-
-        String event = "get";
-        auto& args = script::Function::varArgs();
-        if (args.size() >= 2)
-            event = args[1].str();
-
         code = 0;
         bytes.clear();
 
-#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
         static std::regex expr("^(https?://[^/]+)(.*$)");
         std::cmatch match;
         std::regex_match(url.c_str(), match, expr);
         if (match.empty())
             return -1;
         httplib::Client req{match[1].str().c_str()};
-        auto resp = req.Get(match[2].str().c_str());
+        auto resp = type == Type::Get ? req.Get(match[2].str().c_str()) :
+            req.Post(match[2].str().c_str(),
+                     body.c_str(),
+                     body.size(),
+                     contentType.c_str()
+                );
         if (!resp) {
             error = httplib::to_string(resp.error());
+            logE((int)type, " ", url, " ", contentType);
             return -1;
         }
         bytes.insert(bytes.end(), resp->body.begin(), resp->body.end());
         return int(resp->status);
-#else
-        http::Request req{url};
-        auto resp = req.send("GET");
-        bytes = std::move(resp.body);
-        return int(resp.status.code);
-#endif
     }
 };
 
